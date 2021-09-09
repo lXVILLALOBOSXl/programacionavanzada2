c0d3e749251089f5833184a176f68ce3f0c0b816
Luis Villalobos <ivillalobosrivera@yahoo.com>
2021-09-09 00:27:48 -0500

    comentarios listos, faltan casos similares

--- 1er parcial/P1.3b PA_Peliculas 18100586/src/com/amigos/Maraton.java
+++ 1er parcial/P1.3b PA_Peliculas 18100586/src/com/amigos/Maraton.java
@@ -1,13 +1,24 @@
 package com.amigos;
 
+import java.text.Format;
 import java.time.Duration;
 import java.util.*;
 
+import static java.lang.String.format;
+
+/***
+ * Representa un Maraton que tiene, un periodo de tiempo (duracion), un numero de peliculas (numeroDePeliculas)
+ * y una llista de peliculas(peliculas)
+ */
 public class Maraton {
     private Duration duracion;
     private int numeroDePeliculas;
     private List<Pelicula> peliculas;
-    private List<Duration> posiblesSumas;
+    /***
+     * Se utiliza para almacenar las posibles sumas de periodos de tiempo de la lista de peliculas
+     * Relaciona dicha posible suma con la lista de Peliculas que regresa dicha suma, se indica que se ordena
+     * de mayor a menor rango de tiempo
+     */
     TreeMap <Duration,List<Pelicula>>contenedor = new TreeMap<>(Collections.reverseOrder());
 
     public Maraton(Duration duracion, List<Pelicula> peliculas) {
@@ -38,31 +49,86 @@ public class Maraton {
 
     public void organizate(){
         this.sum(0, Duration.ZERO, new ArrayList<Pelicula>() );
+        /***
+         * Mientras la resta entre el tiempo de maratonn y la mayor suma de las sumas posibles de
+         * negativo, quiere decir que la mayor suma posibles es mayor que la duracion del maraton,
+         * se descarta como posible combinacion y se elimina del mapa
+         * si alguna combinacion es menor que al tiempo mayor quiere decir que es la mejor opcion
+         * ya que se ordena de mayor a menor
+         */
         while (this.duracion.minus(contenedor.firstKey().abs()).isNegative()){
             contenedor.remove(contenedor.firstKey());
         }
         printListMovies((ArrayList<Pelicula>) contenedor.get(contenedor.firstKey()));
     }
 
+    /***
+     * Se encarga de realizar todas las posibles sumas y agregar cada posible suma al mapa y lo relaciona
+     * con la lista de peliculas que regresa dicha suma de tiempo
+     * @param l iteracion dentro de la recursividad
+     * @param count suma de tiempos de las peliculas
+     * @param peliculas lista de peliculas que se asocia con la suma de tiempo
+     */
     public void sum(int l, Duration count, ArrayList<Pelicula> peliculas) {
+        /***
+         * Si la iteracion es del numero de peliculas
+         * Se almacenan la lista de peliculas que se asocia con la suma
+         * que se realiza en la recursvidad
+         * y regresa al punto donde se quedo
+         */
         if(l > (this.peliculas.size()-1)){
             ArrayList<Pelicula> posiblesPeliculas = new ArrayList<Pelicula>(peliculas);
             this.contenedor.put(count,posiblesPeliculas);
             return;
         }
+        /***
+         * Se agrega la pelicula en el index de la iteracion actual
+         */
         peliculas.add(this.peliculas.get(l));
+        /***
+         * Se suma uno a la iteracion y se hace la suma de la cuenta que se lleva mas el tiempo de la pelicula en la iteracion
+         */
         sum(l+1, count.plus(this.peliculas.get(l).getDuracion()), new ArrayList<Pelicula>(peliculas));
+        /***
+         * Se tiene que eliminar la utlima pelicula que se agrego
+         * ya que se retoma la lista de peliculas de la cuenta anterior
+         */
         peliculas.remove(peliculas.size()-1);
+        /***
+         * Se llama el metodo con la cuenta anterior y una iteracion mas
+         * para agregar la lista de pelicula y suma y continuar con las demas sumas
+         */
         sum(l+1,count, new ArrayList<Pelicula>(peliculas));
     }
 
+    /**
+     * Imprime la lista de peliculas de salda
+     * @param peliculasParaVer lista peliculas que han sido elegidas como la mejor opcion para ver en el maraton
+     */
     private void printListMovies(ArrayList<Pelicula> peliculasParaVer){
         System.out.println();
         for (Pelicula pelicula :
                 peliculasParaVer) {
-            System.out.println(pelicula.toString());
+            System.out.println(pelicula.getNombre() + " " + formatDuration(pelicula.getDuracion()));
         }
     }
+
+    /***
+     * Convierte Duration a String en HH:mm
+     * @param duration El tiempo que dura una pelicula en la lista de las peliculas para ver
+     * @return String en formato HH:mm
+     */
+    private static String formatDuration(Duration duration) {
+        long seconds = duration.getSeconds();
+        long absSeconds = Math.abs(seconds);
+        String positive = String.format(
+                "%d:%02d",
+                absSeconds / 3600,
+                (absSeconds % 3600) / 60,
+                absSeconds % 60);
+        return seconds < 0 ? "-" + positive : positive;
+    }
+
 
     @Override
     public String toString() {
--- 1er parcial/P1.3b PA_Peliculas 18100586/src/com/main/Test.java
+++ 1er parcial/P1.3b PA_Peliculas 18100586/src/com/main/Test.java
@@ -18,32 +18,50 @@ public class Test {
         maraton.organizate();
     }
 
+    /***
+     * Pide al usuario el peridodo de tiempo
+     * @return El periodo de tiempo (Duration) del maraton.
+     */
     public static Duration getDuracionMaraton() {
         Scanner scanner = new Scanner(System.in);
+        //Almacena el periodo de tiempo y se inicia en 0 para sacar a diferencia entre el tiempo
+        //que se ingreso
         Duration duracion = Duration.ZERO;
+        //Indica si hay un error al momento de ingresar el dato
         boolean error = false;
 
         do {
             try {
                 scanner = new Scanner(System.in);
                 System.out.print("Ingrese el TIEMPO (HH:mm) del Maraton: ");
+                //Almacena la diferencia de tiempo entre (0 y el tiempo ingresado con un formato HH:mm
                 duracion = Duration.between(LocalTime.MIN, LocalTime.parse(scanner.nextLine(), DateTimeFormatter.ofPattern("H:mm")));
+                //No hay error
                 error = false;
-            } catch (Exception ex) {
+            } catch (Exception ex) { //Se se produce una excepcion
                 System.out.println("ingrese un formato correcto: HH:mm");
+                //Indica que existio un error
                 error = true;
             }
-        }while (error);
+        }while (error); //Se vuelven a solicitar los datos
 
 
         return duracion;
     }
 
+    /***
+     * Pide al usuario el nombre y duracion de la pelicula, lo almacena en una lista de objetos tipo Pelicula
+     * @param numeroDePeliculas
+     * @return Lista de objetos Pelicula ingresada por el usuario
+     */
     public static List<Pelicula> getListaDePeliculas(int numeroDePeliculas) {
+        //Almacena objetos de Pelicula
         List<Pelicula> peliculas = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        //LLena la lista de peliculas de acuero al numero de peliculas que planea ver el usuario
         for (int i = 0; i < numeroDePeliculas; i++) {
+            //Cada numero de pelicula en la lista se tiene que crear desde 0
             Pelicula pelicula = new Pelicula();
             try {
                 System.out.print("\nIngrese el TIEMPO (HH:mm) de la pelicula " + (i + 1) + " : ");
@@ -51,8 +69,9 @@ public class Test {
                 System.out.print("Ingrese el NOMBRE de la pelicula " + (i + 1) + " : ");
                 pelicula.setNombre(scanner.nextLine());
                 peliculas.add(pelicula);
-            } catch (Exception ex) {
+            } catch (Exception ex) { //Si hubo un error al momento de ingresar los datos
                 System.out.println("ingrese un formato correcto: HH:mm");
+                //Volvemos a pedir el dato en el numero de pelicula que hubo en un error
                 i--;
             }
         }
@@ -60,6 +79,10 @@ public class Test {
         return peliculas;
     }
 
+    /***
+     * Pide al usuario el numero de peliculas que pretende ver en el maraton
+     * @return el numero de peliculas que pretende ver el usuario en el maraton
+     */
     public static int getNumeroDePeliculas() {
         Scanner scanner;
         int numeroDePeliculas = 0;
@@ -71,7 +94,9 @@ public class Test {
                 System.out.print("Ingresa el NUMERO de peliculas: ");
                 numeroDePeliculas = scanner.nextInt();
                 error = false;
+                //Si planea ver menos de 1 pelicula (0)
                 if (numeroDePeliculas < 1){
+                    //arroja una excepcion para indicar que se necesita ver al menos una pelicula
                     throw new Exception();
                 }
             } catch (Exception ex) {
